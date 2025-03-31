25931bae66605a7cb6d4fd6d46e5b0d51c2484b5
Eli-Keli <muthokaelikeli@gmail.com>
2025-03-31 22:41:53 +0300

    refactor: Setup project with Expo router and Typescript

--- constants/images.ts
+++
@@ -1,9 +0,0 @@
-const images = {
-  intro: require('../assets/images/Intro.png'),
-  marketplace: require('../assets/images/marketplace.jpeg'),
-  healthcare: require('../assets/images/healthcare.jpeg'),
-  community: require('../assets/images/community.jpeg'),
-  insurance: require('../assets/images/insurance.jpeg'),
-};
-
-export default images;
--- store/store.ts
+++
@@ -1,6 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-
-// We'll add our reducers here later
-export default configureStore({
-  reducer: {}
-})
