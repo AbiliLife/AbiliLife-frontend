63b36b9f8e87c6c1291496ac4a233a50f7042772
Eli-Keli <muthokaelikeli@gmail.com>
2025-04-18 12:20:26 +0300

    fix: correct filename for assistive robot image in Images.ts

--- constants/Images.ts
+++ constants/Images.ts
@@ -2,7 +2,7 @@ const marketplace = require("@/assets/images/marketplace.jpeg");
 const healthcare = require("@/assets/images/healthcare.jpeg");
 const insurance = require("@/assets/images/insurance.jpeg");
 const community = require("@/assets/images/community.jpeg");
-const assistiveRobot = require("@/assets/images/assistive_tech_robot.jpeg");
+const assistiveRobot = require("@/assets/images/assistive-tech-robot.jpeg");
 
 export const images = {
     marketplace,
