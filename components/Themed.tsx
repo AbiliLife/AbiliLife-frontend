b13c28039b89978e730af011e4c5bdb4a9abed3c
Eli-Keli <muthokaelikeli@gmail.com>
2025-05-13 14:00:58 +0300

    fix: remove redundant import of useColorScheme from local file

--- components/Themed.tsx
+++ components/Themed.tsx
@@ -1,8 +1,6 @@
-import { Text as DefaultText, View as DefaultView } from 'react-native';
+import { Text as DefaultText, View as DefaultView, useColorScheme } from 'react-native';
 
 import Colors from '@/constants/Colors';
-import { useColorScheme } from './useColorScheme';
-
 type ThemeProps = {
   lightColor?: string;
   darkColor?: string;
