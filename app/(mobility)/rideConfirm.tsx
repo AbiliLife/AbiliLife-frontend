4f0adad2accc105e3969650713d217c0e813e7ce
Eli-Keli <muthokaelikeli@gmail.com>
2025-05-28 14:44:57 +0300

    feat: add RideConfirmation and RideHistory screen components

---
+++ app/(mobility)/rideConfirm.tsx
@@ -0,0 +1,20 @@
+// Show summary before request is sent
+/* 
+- Show summary: vehicle, route, payment (free for now)
+- "Book via WhatsApp" (link to Ace) or mock button
+*/
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RideConfirmation = () => {
+  return (
+    <View>
+      <Text>RideConfirmation</Text>
+    </View>
+  )
+}
+
+export default RideConfirmation
+
+const styles = StyleSheet.create({})
---
+++ app/(mobility)/rideHistory.tsx
@@ -0,0 +1,16 @@
+// View past rides or scheduled rides
+
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const RideHistory = () => {
+  return (
+    <View>
+      <Text>RideHistory</Text>
+    </View>
+  )
+}
+
+export default RideHistory
+
+const styles = StyleSheet.create({})
