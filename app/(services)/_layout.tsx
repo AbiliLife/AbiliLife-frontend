8a5b023d27bea52a82ae942611d1f3a2f9cd2c69
Eli-Keli <muthokaelikeli@gmail.com>
2025-06-12 18:02:54 +0300

    MERGE 102 commits into main from native/navtypescript

---
+++ app/(services)/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router';
+
+export default function ServicesLayout() {
+    return (
+        <Stack>
+          <Stack.Screen name="healthcare" options={{ headerShown: false }} />
+          <Stack.Screen name="education" options={{ headerShown: false }} />
+          <Stack.Screen name="employment" options={{ headerShown: false }} />
+          <Stack.Screen name="insurance" options={{ headerShown: false }} />
+          <Stack.Screen name="doctors/[doc]" options={{ headerShown: false, title: "Doctor Details" }} />
+        </Stack>
+    );
+}
