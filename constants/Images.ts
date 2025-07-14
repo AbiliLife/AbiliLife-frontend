4e138b7fffd150c4ff5e267728b4191888108f77
Eli-Keli <muthokaelikeli@gmail.com>
2025-07-14 21:56:19 +0300

    FT: add new logo images for accessible travel and related services

--- constants/Images.ts
+++ constants/Images.ts
@@ -7,6 +7,11 @@ const mobility = require("@/assets/images/mobility.png");
 const welcome = require("@/assets/images/welcome.png");
 const login = require("@/assets/images/login.png");
 const signup = require("@/assets/images/sign-up.png");
+const caregiver = require("@/assets/images/caregiver.png");
+const aceLogo = require("@/assets/images/ace-logo.png");
+const bammToursLogo = require("@/assets/images/bamm-tours-logo.png");
+const accessibleTravelLogo = require("@/assets/images/accessible-travel-logo.png");
+const nbAirportLogo = require("@/assets/images/nb-airport-logo.png");
 
 export const images = {
     marketplace,
@@ -18,4 +23,9 @@ export const images = {
     welcome,
     login,
     signup,
+    caregiver,
+    aceLogo,
+    bammToursLogo,
+    accessibleTravelLogo,
+    nbAirportLogo,
 }
