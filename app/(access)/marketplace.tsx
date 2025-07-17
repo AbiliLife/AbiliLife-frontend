fa2107964f7a92cb0dcb699292308beae7eab5ac
Eli-Keli <muthokaelikeli@gmail.com>
2025-07-17 18:41:12 +0300

    FT: implement Marketplace Module with header and layout

--- app/(access)/marketplace.tsx
+++ app/(access)/marketplace.tsx
@@ -1,11 +1,23 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import ModuleHeader from '@/components/common/ModuleHeader';
+import React from 'react';
+import { useRouter } from 'expo-router';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import Colors from '@/constants/Colors';
 
 const MarketplaceModule = () => {
+  const router = useRouter();
   return (
-    <View>
-      <Text>MarketplaceModule</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1 }} edges={['top', 'left', 'right']}>
+      {/* Header */}
+      <ModuleHeader
+        title="AbiliLife Access"
+        subtitle="Find and explore a variety of products and services to enhance your life."
+        onBackPress={() => router.back()}
+        color={Colors.orange}
+        iconName='shopping-cart'
+        iconFamily='FontAwesome'
+      />
+    </SafeAreaView>
   )
 }
 
