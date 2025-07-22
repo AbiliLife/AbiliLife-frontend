cc5c659ffa27c24a37e8981bcd98e079463d78bb
Eli-Keli <muthokaelikeli@gmail.com>
2025-07-22 20:00:17 +0300

    refactor: update profileSetup screen options for improved theme handling

--- app/(onboarding)/_layout.tsx
+++ app/(onboarding)/_layout.tsx
@@ -8,24 +8,36 @@ import Colors from '@/constants/Colors';
 export default function OnboardingLayout() {
   const { currentTheme } = React.useContext(ThemeContext);
   return (
-      <Stack>
-        <Stack.Screen name="onboard" options={{ headerShown: false }} />
-        <Stack.Screen name="welcome" options={{ headerShown: false }} />
-        <Stack.Screen name="profileSetup" options={{ headerShown: false }} />
-        <Stack.Screen name="bookingGuide" options={{
-          headerTitle: 'How to Book a Ride',
-          headerTitleAlign: 'center',
-          headerShadowVisible: false,
-          headerTitleStyle: {
-            fontSize: 20,
-            fontWeight: '600',
-            color: currentTheme === 'light' ? Colors.blue : Colors.white,
-          },
-          headerStyle: {
-            backgroundColor: currentTheme === 'light' ? Colors.lightContainer : Colors.darkContainer,
-          },
-          headerRight: () => <Ionicons name="help-circle-outline" size={32} color={currentTheme === 'light' ? Colors.blue : Colors.white}  />,
-        }} />
-      </Stack>
+    <Stack>
+      <Stack.Screen name="onboard" options={{ headerShown: false }} />
+      <Stack.Screen name="welcome" options={{ headerShown: false }} />
+      <Stack.Screen name="profileSetup" options={{
+        headerTitle: 'Profile Setup',
+        headerShadowVisible: false,
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          color: currentTheme === 'light' ? Colors.primary : Colors.white,
+        },
+        headerStyle: {
+          backgroundColor: currentTheme === 'light' ? Colors.lightContainer : Colors.darkContainer,
+        },
+        headerTintColor: currentTheme === 'light' ? Colors.primary : Colors.white,
+        headerBackButtonDisplayMode: 'minimal',
+      }} />
+      <Stack.Screen name="bookingGuide" options={{
+        headerTitle: 'How to Book a Ride',
+        headerTitleAlign: 'center',
+        headerShadowVisible: false,
+        headerTitleStyle: {
+          fontSize: 20,
+          fontWeight: '600',
+          color: currentTheme === 'light' ? Colors.blue : Colors.white,
+        },
+        headerStyle: {
+          backgroundColor: currentTheme === 'light' ? Colors.lightContainer : Colors.darkContainer,
+        },
+        headerRight: () => <Ionicons name="help-circle-outline" size={32} color={currentTheme === 'light' ? Colors.blue : Colors.white} />,
+      }} />
+    </Stack>
   );
 }
