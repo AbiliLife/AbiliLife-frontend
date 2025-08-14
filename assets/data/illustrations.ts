fa2f13b08601afd0891e74b23ea5256cabc62875
Eli-Keli <muthokaelikeli@gmail.com>
2025-08-14 15:14:20 +0300

    feat: update illustration assets for improved visual representation

--- assets/data/illustrations.ts
+++ assets/data/illustrations.ts
@@ -3,7 +3,9 @@ export default {
     logo: 'https://images.unsplash.com/photo-1589149098258-3b71a54060f7?q=80&w=300&auto=format&fit=crop',
     welcome: 'https://www.globalgiving.org/pfil/36055/pict_large.jpg',
     accessibleRide: 'https://roamingafricatours.com/wp-content/uploads/2024/06/Disability-Tours-scaled.jpg',
-    bookRide: 'https://images.unsplash.com/photo-1449965408869-eaa3f722e40d?q=80&w=800&auto=format&fit=crop',
-    preferences: 'https://images.unsplash.com/photo-1484480974693-6ca0a78fb36b?q=80&w=800&auto=format&fit=crop',
-    support: 'https://images.unsplash.com/photo-1582213782179-e0d53f98f2ca?q=80&w=800&auto=format&fit=crop',
+    mobilityOption: 'https://dims.apnews.com/dims4/default/b352b8f/2147483647/strip/true/crop/5457x3638+0+0/resize/599x399!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F5d%2F95%2F936a8a338c6e01a9541dfa7e3746%2F39124a04d7ae4fb79d67aff8960fed7d',
+    bookRide: 'https://roamingafricatours.com/wp-content/uploads/2024/06/Disability-Tours-scaled.jpg',
+    caregiver: 'https://1plus1cares.com/wp-content/uploads/2024/06/65.jpg',
+    preferences: 'https://images.unsplash.com/photo-1585244129648-5dc1f9cd9d7a?q=80&w=1470&auto=format&fit=crop',
+    support: 'https://img.freepik.com/premium-photo/portrait-customer-service-support-with-black-woman-consulting-using-headset-office-work-contact-us-crm-assistance-with-female-consultant-agent-receptionist-ready-help_590464-175489.jpg',
 };
